affc0d719987cc42382cdf9f5a75eba2fc77f2df
AndersonGomezAza <argomeza@itc.edu.co>
2023-09-06 13:11:27 -0500

    Fix Error BackEnd Api

--- GymEtitc/Program.cs
+++ GymEtitc/Program.cs
@@ -1,3 +1,6 @@
+using GymEtitc.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +17,7 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<GymEtitcContext>(options => options.UseSqlServer("Server=DESKTOP-HNG3HQ1\\SQLEXPRESS; Database=GymEtitc; Trusted_Connection=true;MultipleActiveResultSets=true"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
