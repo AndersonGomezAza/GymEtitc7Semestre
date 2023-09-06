9b2c5cb2e9494fd24ed6525911722d13e885098a
KevinBello11 <kebelloc@itc.edu.co>
2023-09-06 14:52:16 -0500

    Uploading controllers Kevin

---
+++ GymEtitc/Controllers/RutinasController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GymEtitc.Models;
+
+namespace GymEtitc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RutinasController : ControllerBase
+    {
+        private readonly GymEtitcContext _context;
+
+        public RutinasController(GymEtitcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Rutinas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rutinas>>> GetRutinas()
+        {
+          if (_context.Rutinas == null)
+          {
+              return NotFound();
+          }
+            return await _context.Rutinas.ToListAsync();
+        }
+
+        // GET: api/Rutinas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rutinas>> GetRutinas(int id)
+        {
+          if (_context.Rutinas == null)
+          {
+              return NotFound();
+          }
+            var rutinas = await _context.Rutinas.FindAsync(id);
+
+            if (rutinas == null)
+            {
+                return NotFound();
+            }
+
+            return rutinas;
+        }
+
+        // PUT: api/Rutinas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRutinas(int id, Rutinas rutinas)
+        {
+            if (id != rutinas.IdRutina)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rutinas).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RutinasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rutinas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Rutinas>> PostRutinas(Rutinas rutinas)
+        {
+          if (_context.Rutinas == null)
+          {
+              return Problem("Entity set 'GymEtitcContext.Rutinas'  is null.");
+          }
+            _context.Rutinas.Add(rutinas);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RutinasExists(rutinas.IdRutina))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRutinas", new { id = rutinas.IdRutina }, rutinas);
+        }
+
+        // DELETE: api/Rutinas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRutinas(int id)
+        {
+            if (_context.Rutinas == null)
+            {
+                return NotFound();
+            }
+            var rutinas = await _context.Rutinas.FindAsync(id);
+            if (rutinas == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rutinas.Remove(rutinas);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RutinasExists(int id)
+        {
+            return (_context.Rutinas?.Any(e => e.IdRutina == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ GymEtitc/Controllers/UsuariosController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GymEtitc.Models;
+
+namespace GymEtitc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly GymEtitcContext _context;
+
+        public UsuariosController(GymEtitcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuarios>>> GetUsuarios()
+        {
+          if (_context.Usuarios == null)
+          {
+              return NotFound();
+          }
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuarios>> GetUsuarios(int id)
+        {
+          if (_context.Usuarios == null)
+          {
+              return NotFound();
+          }
+            var usuarios = await _context.Usuarios.FindAsync(id);
+
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            return usuarios;
+        }
+
+        // PUT: api/Usuarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuarios(int id, Usuarios usuarios)
+        {
+            if (id != usuarios.NumDocumento)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuarios).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuariosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Usuarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Usuarios>> PostUsuarios(Usuarios usuarios)
+        {
+          if (_context.Usuarios == null)
+          {
+              return Problem("Entity set 'GymEtitcContext.Usuarios'  is null.");
+          }
+            _context.Usuarios.Add(usuarios);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsuariosExists(usuarios.NumDocumento))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetUsuarios", new { id = usuarios.NumDocumento }, usuarios);
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuarios(int id)
+        {
+            if (_context.Usuarios == null)
+            {
+                return NotFound();
+            }
+            var usuarios = await _context.Usuarios.FindAsync(id);
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuarios);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsuariosExists(int id)
+        {
+            return (_context.Usuarios?.Any(e => e.NumDocumento == id)).GetValueOrDefault();
+        }
+    }
+}
